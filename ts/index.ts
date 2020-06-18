37c84a4dcec3bebe44522caaff9cea25cec4e9aa
rui-watanabe <wataru610806@gmail.com>
2020-06-18 16:54:25 +0900

    add uuid function

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,5 @@
 import { User } from './User';
+import { v4 as uuidv4 } from "uuid";
 
 const user = new User('tanaka', 'taro', 44);
 
@@ -6,4 +7,11 @@ const contentsElement = document.getElementById('contents');
 if(!!contentsElement)
 {
     contentsElement.innerText = `${user.familyName} ${user.firstName}`;
+}
+
+const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+
+saibanButton.onclick = (e) => {
+    uuidSpan.innerText = uuidv4();
 }
